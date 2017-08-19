f30445c06b473a399ff9a551b725d5c26365e61b
Malsha-Perera <thiliniprobo11216@gmail.com>
2017-08-19 16:24:50 +0530

    initial commit

---
+++ src/GUI/login_controller.java
@@ -0,0 +1,19 @@
+package GUI;/**
+ * Created by acer on 8/18/2017.
+ */
+
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class login_controller {
+
+
+    public PasswordField PwdTx;
+    public TextField UserTx;
+    public Button SignBt;
+
+}
