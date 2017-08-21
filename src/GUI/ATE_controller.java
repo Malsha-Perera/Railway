0292b53726ca1fc548fc43ab394aa6c66489319f
Thilini_Fernando <tpfernando.94@gmail.com>
2017-08-22 00:17:34 +0530

    ATE main interface

---
+++ src/GUI/ATE_controller.java
@@ -0,0 +1,10 @@
+package GUI;
+
+import javafx.event.ActionEvent;
+
+public class ATE_controller {
+    public void PressBtn1(ActionEvent event){}
+    public  void PressBtn2(ActionEvent event){}
+    public void PressBtn3(ActionEvent event){}
+
+}
