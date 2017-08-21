ec92d60ef86213eaec77733a49edaec73201f504
Thilini_Fernando <tpfernando.94@gmail.com>
2017-08-21 21:54:39 +0530

    Chief engineer main interface

---
+++ src/GUI/ChiefEng_controller.java
@@ -0,0 +1,20 @@
+package GUI;
+
+import javafx .event.ActionEvent;
+
+public class ChiefEng_controller {
+    public void PressButton1(ActionEvent event){
+
+    }
+
+    public void PressButton2(ActionEvent event){
+
+    }
+    public void PressButton3(ActionEvent event){
+
+    }
+
+
+
+
+}
